402179e36b97a43825eb7bbf63ba75bd249f9bd8
Billy Peters <c3055481@newcastle.ac.uk>
2024-04-23 21:28:24 +0100

    Added PreviousQualification entity

---
+++ src/main/java/com/dysonstudentmanagement/dsm/entity/studenttutor/StudentTutor.java
@@ -0,0 +1,35 @@
+package com.dysonstudentmanagement.dsm.entity.studenttutor;
+
+import com.dysonstudentmanagement.dsm.entity.user.UserPrimaryData;
+import com.dysonstudentmanagement.dsm.entity.useremergencycontact.UserEmergencyContactCompositeKey;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Entity
+@Table(name = "StudentTutor")
+@IdClass(StudentTutorCompositeKey.class)
+public class StudentTutor{
+    @Id
+    @Column(name = "StudentID", length = 8)
+    private String studentID;
+    @Id
+    @Column(name = "StaffID", length = 8)
+    private String staffID;
+
+    @MapsId
+    @OneToOne
+    @JoinColumn(name = "StudentID", referencedColumnName = "UserID")
+    private UserPrimaryData studentPrimaryData;
+
+    @MapsId
+    @ManyToOne
+    @JoinColumn(name = "StaffID", referencedColumnName = "UserID")
+    private UserPrimaryData staffPrimaryData;
+
+}
