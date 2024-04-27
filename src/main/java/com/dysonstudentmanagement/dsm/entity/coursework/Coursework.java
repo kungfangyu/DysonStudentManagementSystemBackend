e0d044c81b3f8aef0d222da6f4b7dcc3a0702f88
jackdraz <jackwillburnett@gmail.com>
2024-04-27 12:53:12 +0100

    coursework objects implemented+tested

---
+++ src/main/java/com/dysonstudentmanagement/dsm/entity/coursework/Coursework.java
@@ -0,0 +1,42 @@
+package com.dysonstudentmanagement.dsm.entity.coursework;
+
+import com.dysonstudentmanagement.dsm.entity.moduledetails.ModuleDetails;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Type;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Entity
+@Table(name = "Coursework")
+@IdClass(CourseworkCompositeKey.class)
+public class Coursework {
+    @Id
+    @Column(name = "ModuleID")
+    private String moduleID;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "CourseworkID")
+    private int courseworkID;
+    @Column(name = "Description")
+    private String description;
+    @Column(name = "Deadline")
+    private Timestamp deadline;
+    @Column(name = "PercentageOfModule")
+    private Double percentageOfModule;
+    @Column(name = "isCourseworkPublished",columnDefinition = "TINYINT", length = 1)
+    private boolean isCourseworkPublished;
+    @Column(name = "isGradePublished", columnDefinition = "TINYINT", length = 1)
+    private boolean isGradePublished;
+
+    @MapsId
+    @ManyToOne
+    @JoinColumn(name = "ModuleID", referencedColumnName = "ModuleID")
+    private ModuleDetails moduleDetails;
+}
---
+++ src/main/java/com/dysonstudentmanagement/dsm/entity/coursework/CourseworkCompositeKey.java
@@ -0,0 +1,37 @@
+package com.dysonstudentmanagement.dsm.entity.coursework;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourseworkCompositeKey implements Serializable {
+    @Id
+    @Column(name = "ModuleID")
+    private String moduleID;
+    @Id
+    @Column(name = "CourseworkID")
+    private int courseworkID;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CourseworkCompositeKey that = (CourseworkCompositeKey) o;
+        return getCourseworkID() == that.getCourseworkID() && Objects.equals(getModuleID(), that.getModuleID());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getModuleID(), getCourseworkID());
+    }
+}
