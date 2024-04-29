9f0a62664a0fa89e7a65071f428dce878f547b72
FangyuKung <fish19921026@gmail.com>
2024-04-29 01:10:27 +0100

    fix: cross problems and user login response

--- src/main/java/com/dysonstudentmanagement/dsm/controller/UserLogInController.java
+++ src/main/java/com/dysonstudentmanagement/dsm/controller/UserLogInController.java
@@ -2,14 +2,18 @@ package com.dysonstudentmanagement.dsm.controller;
 
 import com.dysonstudentmanagement.dsm.dto.AuthResponseDto;
 import com.dysonstudentmanagement.dsm.dto.UserLogInDto;
+import com.dysonstudentmanagement.dsm.dto.user.UserPrimaryDataDto;
+import com.dysonstudentmanagement.dsm.entity.user.UserPrimaryData;
 import com.dysonstudentmanagement.dsm.entity.userlogin.UserLogIn;
 import com.dysonstudentmanagement.dsm.exception.InvalidUserTypeException;
 import com.dysonstudentmanagement.dsm.repository.UserPrimaryDataRepository;
 import com.dysonstudentmanagement.dsm.service.UserLogInService;
 import com.dysonstudentmanagement.dsm.security.JwtUtils;
+import com.dysonstudentmanagement.dsm.service.UserPrimaryDataService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,17 +24,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserLogInController {
     private final UserLogInService userLogInService;
 
+    private final UserPrimaryDataService userPrimaryDataService;
+
     private final JwtUtils jwtUtils;
 
-    private UserPrimaryDataRepository userPrimaryDataRepository;
-
-    public UserLogInController(UserLogInService userLogInService, JwtUtils jwtUtils
+    public UserLogInController(UserLogInService userLogInService, JwtUtils jwtUtils,
+                               UserPrimaryDataService userPrimaryDataService
                                ) {
         this.userLogInService = userLogInService;
         this.jwtUtils = jwtUtils;
+        this.userPrimaryDataService = userPrimaryDataService;
     }
 
+    @CrossOrigin(origins = "http://localhost:3000/")
     @PostMapping("/login")
+
     public ResponseEntity<?> createUserLogin(@RequestBody UserLogInDto userLoginDto) {
 
         try {
@@ -41,8 +49,17 @@ public class UserLogInController {
             if(userLogin == null){
                 throw new InvalidUserTypeException("UserID or Password is Invalid");
             }
+            UserPrimaryDataDto userPrimaryDataDto = userPrimaryDataService.getUserPrimaryDataById(userLoginDto.getUserID());
+
+            AuthResponseDto authResponseDto = new AuthResponseDto();
+            authResponseDto.setUserID(userPrimaryDataDto.getUserID());
+            authResponseDto.setDysonEmail(userPrimaryDataDto.getDysonEmail());
+            authResponseDto.setFirstName(userPrimaryDataDto.getFirstName());
+            authResponseDto.setLastName(userPrimaryDataDto.getLastName());
+            authResponseDto.setUserType(userPrimaryDataDto.getUserType());
             String token = jwtUtils.generateJwt(userLoginDto.getUserID());
-            return ResponseEntity.ok(token);
+            authResponseDto.setAccessToken(token);
+            return ResponseEntity.ok(authResponseDto);
         } catch (InvalidUserTypeException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
         }
--- src/main/java/com/dysonstudentmanagement/dsm/dto/AuthResponseDto.java
+++ src/main/java/com/dysonstudentmanagement/dsm/dto/AuthResponseDto.java
@@ -1,13 +1,38 @@
 package com.dysonstudentmanagement.dsm.dto;
 
-import lombok.Data;
+import com.dysonstudentmanagement.dsm.entity.user.UserPrimaryData;
+import lombok.*;
+
+import java.time.LocalDate;
 
 @Data
+@Getter
+@Setter
+//@AllArgsConstructor
+@ToString
 public class AuthResponseDto {
     private String accessToken;
-    private String tokenType = "Bearer ";
+    private String userID;
+    private UserPrimaryData.UserType userType;
+    private String firstName;
+    private String lastName;
+    private String dysonEmail;
 
-    public AuthResponseDto(String accessToken) {
+    public AuthResponseDto(String accessToken, String userID,
+                           UserPrimaryData.UserType userType,
+                           String firstName,
+                           String lastName,
+                           String dysonEmail) {
         this.accessToken = accessToken;
+        this.userID = userID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dysonEmail = dysonEmail;
+        this.userType = userType;
+
+    }
+
+    public AuthResponseDto() {
+
     }
 }
