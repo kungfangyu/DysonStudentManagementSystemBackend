9cead9d8aeef23d45f74fb820bbf5d06ad040728
templelii <tianpu.li@outlook.com>
2024-05-03 10:09:16 +0100

    ModuleMaterial, StudentModuleGrade

--- src/main/java/com/dysonstudentmanagement/dsm/service/impl/ModuleStaffServiceImpl.java
+++ src/main/java/com/dysonstudentmanagement/dsm/service/impl/ModuleStaffServiceImpl.java
@@ -1,11 +1,14 @@
 package com.dysonstudentmanagement.dsm.service.impl;
 
+import com.dysonstudentmanagement.dsm.dto.ModuleDetailsDto;
 import com.dysonstudentmanagement.dsm.dto.ModuleStaffDto;
 import com.dysonstudentmanagement.dsm.entity.moduledetails.ModuleDetails;
 import com.dysonstudentmanagement.dsm.entity.modulestaff.ModuleStaff;
 import com.dysonstudentmanagement.dsm.entity.modulestaff.ModuleStaffCompositeKey;
+import com.dysonstudentmanagement.dsm.entity.studentmodulegrade.StudentModuleGrade;
 import com.dysonstudentmanagement.dsm.entity.user.UserPrimaryData;
 import com.dysonstudentmanagement.dsm.exception.ResourceNotFoundException;
+import com.dysonstudentmanagement.dsm.mapper.ModuleDetailsMapper;
 import com.dysonstudentmanagement.dsm.mapper.ModuleStaffMapper;
 import com.dysonstudentmanagement.dsm.repository.ModuleDetailsRepository;
 import com.dysonstudentmanagement.dsm.repository.ModuleStaffRepository;
@@ -15,6 +18,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -62,5 +66,16 @@ public class ModuleStaffServiceImpl implements ModuelStaffService {
         ModuleStaff moduleStaff = moduleStaffRepository.findById(targetKey)
                 .orElseThrow(() -> new ResourceNotFoundException("Module does not exist"));
         moduleStaffRepository.deleteById(targetKey);
+    }
+
+    @Override
+    public List<ModuleDetailsDto> getModuleDetailsByStaffID(String staffID) {
+        List<ModuleStaff> moduleStaffs = moduleStaffRepository.findByStaffID(staffID);
+        List<ModuleDetails> moduleDetailsList = new LinkedList<>();
+        for(ModuleStaff moduleStaff:moduleStaffs){
+            moduleDetailsList.add(moduleStaff.getModuleDetails());
+        }
+
+        return moduleDetailsList.stream().map(ModuleDetailsMapper::mapToModuleDetailsDto).collect(Collectors.toList());
     }
 }
